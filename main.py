015a63b94c9dd85e5705c9dcd9a04dc7d4f238d3
wilson nie <wilson.nie13@gmail.com>
2019-09-13 13:55:42 -0600

    gitignore file added ignored all *.pyc files

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 ## import local package here
-from ProcessOriginalFile import ProcessFile
-from GenerateOutputFile import GenerateFile
-from ObtainRequiredAttributes import ObtainOpenAPIAttributes
+# from ProcessOriginalFile import ProcessFile
+# from GenerateOutputFile import GenerateFile
+# from ObtainRequiredAttributes import ObtainOpenAPIAttributes
 
 ## main program flow start here
 if __name__ == "__main__":
-    file_dict = ProcessFile.Process("routes.txt");
-    file_dict = ObtainOpenAPIAttributes.Obtain(file_dict);
-    GenerateFile.GenerateAPIDoc(file_dict);
+    print("test")
